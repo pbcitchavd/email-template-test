8b34f7206e4ac398535de3ebb3ab4e7a3a177f56
pbcitchavd <asan.chavdarliev@pbconsult.de>
2023-04-28 21:42:18 +0200

    set staticfiles

--- core_phishing/settings.py
+++ core_phishing/settings.py
@@ -146,6 +146,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
+
+
 MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
