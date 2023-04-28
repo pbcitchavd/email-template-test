b504860ecccbe60fea582079bdbf4a0a23af27de
pbcitchavd <asan.chavdarliev@pbconsult.de>
2023-04-28 22:39:16 +0200

    set .env debuger on

--- app_phishing/urls.py
+++ app_phishing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = "app_phishing"
@@ -17,5 +17,5 @@ urlpatterns = [
     path('query/', views.query_employees, name="query"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
